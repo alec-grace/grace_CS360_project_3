5c48bb4f962270c3511cfc583d95913311a0492b
alec-grace <agrace072@protonmail.com>
2022-04-06 10:55:56 -0400

    Added local_freq method, possibly added working probability for loop, still specific to babbage, need to move to own class/file and abstract. Also added test training data to test small bits.

--- file_reader.py
+++ file_reader.py
@@ -7,8 +7,9 @@
 class FileReader:
     
     def __init__(self, filename: str):
+        self.word_frequencies = {}
         self.total_words = 0
-        author_grams = []
+        self.author_grams = []
         with open(filename, 'r') as author_file:
             lines = author_file.readlines()
             for line in lines:
@@ -16,16 +17,18 @@ class FileReader:
                 line = '<sol>,' + line + ',<eol>'
                 split_line = line.split(',')
                 for i in range(len(split_line) - 1):
-                    author_grams.append((split_line[i], split_line[i + 1]))
+                    self.author_grams.append((split_line[i], split_line[i + 1]))
                 self.total_words += len(split_line)
         author_file.close()
 
         self.word_tracker = {}
-        for gram in author_grams:
+        for gram in self.author_grams:
             locator = False
             if gram[0] not in self.word_tracker.keys():
                 self.word_tracker[gram[0]] = [[gram[1], 1]]
+                self.word_frequencies[gram[0]] = 1
             else:
+                self.word_frequencies[gram[0]] += 1
                 for duo_list in self.word_tracker[gram[0]]:
                     if duo_list[0] == gram[1]:
                         duo_list[1] += 1
@@ -36,3 +39,15 @@ class FileReader:
     def word_result(self, target: str):
         for pair in self.word_tracker[target]:
             print(pair)
+
+    def word_freq(self, word: str):
+        return self.word_frequencies[word] / self.total_words
+
+    def local_freq(self, w1: str, w2: str):
+        total = 0
+        frequency = 0
+        for val_list in self.word_tracker[w1]:
+            total += val_list[1]
+            if val_list[0] == w2:
+                frequency = val_list[1]
+        return frequency / total
--- main.py
+++ main.py
@@ -4,30 +4,48 @@
 # Purpose:
 #   driver for CS360 Project 3: Author Prediction Using Bayesian Analysis
 from file_reader import FileReader
+import math
 
 
 def main():
+    # create training data for each author
+    babbage_train = FileReader('babbage-train.txt')
+    freud_train = FileReader('freud-train.txt')
+    poe_train = FileReader('poe-train.txt')
+    test_train = FileReader('test-train.txt')
 
-    babbage_info = FileReader('babbage-train.txt')
-    freud_info = FileReader('freud-train.txt')
-    poe_info = FileReader('poe-train.txt')
+    # separate the test file into grams
+    babbage_test = FileReader('babbage-test.txt')
 
-    freud_info.word_result('IN')
-    print(babbage_info.total_words)
-    print(freud_info.total_words)
-    print(poe_info.total_words)
+    # testing stuff
+    test_grams = [['HELLO', 'GLOBE'], ['ANTONIO', 'PIZZA'], ['ONE', 'HUNDRED'],
+                  ['IN', 'A'], ['WHICH', 'ARE'], ['DERIVED', 'LITTLE']]
+
+    # set up necessary info before testing files
+    frequencies = []
+    default_probability = 2 ** -15
+    for gram in babbage_test.author_grams:
+    # for gram in test_grams:
+        if gram[0] == '<sol>':
+            frequencies.append(babbage_train.word_freq('<sol>'))
+        elif gram[0] in babbage_train.word_tracker.keys():
+            for val_list in babbage_train.word_tracker[gram[0]]:
+                if val_list[0] == gram[1]:
+                    freq = babbage_train.local_freq(gram[0], gram[1])
+                    frequencies.append(freq)
+        else:
+            frequencies.append(default_probability)
+
+    running_total = 0
+    for thing in frequencies:
+        running_total += math.log(thing)
+
+    print(running_total)
 
 
 if __name__ == "__main__":
     main()
 
-
-# dictionary of all seen words in one corpus:
-# Key: unique word
-# Value: list of lists [2] [next word, # of occurrences]
-#
-# EX: dict = {
-#       'THE' : ['CAT', 2], ['RAT', 4], ['MAT', 1]
 #
 # a^x = y, log(base a) y = x
 # P(a) = 0.125 (1/8)
@@ -41,4 +59,3 @@ if __name__ == "__main__":
 # p(a) * p(b) * p(c) ... = log(a) + log(b) + log(c) ...
 # x > y
 # log(x) > log(y)
-
